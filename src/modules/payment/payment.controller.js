39358657ce127bc19874346ac2a2686b97fcbb83
tanzia mourin <tanziamourin68@gmail.com>
2026-06-22 11:04:37 +0600

    remove duplicated file

--- src/modules/payment/payment.controller.js
+++
@@ -1,8 +0,0 @@
-import { createPaymentIntent } from "../../stripe/stripe.service";
-export const createPayment = async (req, res) => {
-    const { amount } = req.body;
-    const paymentIntent = await createPaymentIntent(amount);
-    res.json({
-        clientSecret: paymentIntent.client_secret
-    });
-};
--- src/modules/payment/payment.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { createPayment } from "./payment.controller";
-const router = Router();
-router.post("/create-payment-intent", createPayment);
-export default router;
