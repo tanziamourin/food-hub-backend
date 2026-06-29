e5fb88d4063560289aa99efd9ce4d5062fe35063
tanzia mourin <tanziamourin68@gmail.com>
2026-06-29 13:41:51 +0600

    remove generated api files2

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 // import app from "../src/app"; 
 // import { createServer } from "http";
 
-import app from "../src/app";
+// import app from "../src/app";
 
 // const server = createServer(app);
 
 // export default server;
 
+
+// export default app;
+import app from "../dist/app.js";
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "./app";
+// import { VercelRequest, VercelResponse } from "@vercel/node";
+// import app from "./app";
 
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
-};
+// export default (req: VercelRequest, res: VercelResponse) => {
+//   app(req, res);
+// };
