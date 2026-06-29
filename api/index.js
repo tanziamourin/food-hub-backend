ff122a57edf5ca27363f5c82b2d405f48b08340b
tanzia mourin <tanziamourin68@gmail.com>
2026-06-29 13:27:27 +0600

    remove generated api files

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// import app from "../src/app"; 
-// import { createServer } from "http";
-import app from "../src/app";
-// const server = createServer(app);
-// export default server;
-export default app;
-//# sourceMappingURL=index.js.map
